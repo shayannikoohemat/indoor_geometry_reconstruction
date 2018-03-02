72e4af293279867b50ae1ea5428b62a32c0a927a
NikoohematS <shayannkh>
2018-03-02 10:39:02 +0100

    adding slanted wall construction

---
+++ cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/Buffer.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/Buffer.cpp.obj"
+  "D:/Mapping/Tools/visualization_tools/MinimumRectangle3D.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/D_/Mapping/Tools/visualization_tools/MinimumRectangle3D.cpp.obj"
+  "D:/Mapping/Tools/visualization_tools/VisualizePlane3D.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/D_/Mapping/Tools/visualization_tools/VisualizePlane3D.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/DepthMap.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/DepthMap.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/FilterSegmentsByTime.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/FilterSegmentsByTime.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/GenerateWallPatches.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/GenerateWallPatches.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/Laserpoint2ScanningPosition.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/Laserpoint2ScanningPosition.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/Laservoxel.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/Laservoxel.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/MLS_preprocessing.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/MLS_preprocessing.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/ReadAscii.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/ReadAscii.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/WallAccuracy.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/WallAccuracy.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/indoorTopology.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/indoorTopology.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/indoorTopology_v1_old.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/indoorTopology_v1_old.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/main.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/main.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/occlusion_test.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/occlusion_test.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/post_processing.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/post_processing.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/segment_refinement.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/segment_refinement.cpp.obj"
+  "D:/Mapping/Tools/indoor_geometry_reocnstruction/setConsoleColor.cpp" "D:/Mapping/Tools/indoor_geometry_reocnstruction/cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/setConsoleColor.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Program Files/boost_gcc4.7/include/boost-1_64"
+  "D:/Mapping/Library/Photogrammetry/include"
+  "D:/Mapping/Library/Buildings/include"
+  "D:/Mapping/Library/LaserScan/include"
+  "D:/Mapping/Foreign/ANN/include/ANN"
+  "D:/Mapping/Foreign/ANN/src"
+  "D:/Mapping/Tools/visualization_tools"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/indoor_reconstruction.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/indoor_reconstruction.dir/main.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/indoorTopology.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/setConsoleColor.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/segment_refinement.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/FilterSegmentsByTime.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/Buffer.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/GenerateWallPatches.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/occlusion_test.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/ReadAscii.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/Laservoxel.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/Laserpoint2ScanningPosition.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/DepthMap.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/WallAccuracy.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/MLS_preprocessing.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/post_processing.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/D_/Mapping/Tools/visualization_tools/VisualizePlane3D.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/D_/Mapping/Tools/visualization_tools/MinimumRectangle3D.cpp.obj"
+  "CMakeFiles/indoor_reconstruction.dir/indoorTopology_v1_old.cpp.obj"
+  "../Debug/indoor_reconstruction.pdb"
+  "../Debug/indoor_reconstruction.exe"
+  "../Debug/indoor_reconstruction.exe.manifest"
+  "libindoor_reconstruction.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/indoor_reconstruction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
